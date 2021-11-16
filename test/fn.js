0c423e0c6196daccb0f98b2d1630d208aeb39170
2heeesss <hohooodo@gmail.com>
2021-11-16 21:26:59 +0900

    Test: remove continue keyword in isOneOrTwoMajor function

--- test/fn.js
+++ test/fn.js
@@ -1,20 +1,17 @@
 const userData = require('./mydata');
 
 const fn = {
-    isOneOrTwoMajor: (data) => {
-        for (const cell of data) {
-            if (!cell.__EMPTY_28) {
-                continue;
-            }
-
-            if (cell.__EMPTY_28.includes('복수전공')) {
-                return '복수전공';
-            } else if (cell.__EMPTY_28.includes('부전공')) {
-                return '부전공';
-            }
-        }
-        return '전공';
-    },
+  isOneOrTwoMajor: (data) => {
+    for (const cell of data) {
+      if (cell.__EMPTY_28.includes('복수전공')) {
+        return '복수전공';
+      }
+      if (cell.__EMPTY_28.includes('부전공')) {
+        return '부전공';
+      }
+    }
+    return '전공';
+  },
 };
 
 module.exports = fn;
